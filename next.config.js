9b2f50433ba4bb74640d0622f1edb9a64fc2c5dc
artemtkachev <tkachev.tech@yandex.ru>
2025-05-13 00:49:39 +0300

    fff

--- next.config.js
+++ next.config.js
@@ -1,1 +1,18 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/start',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig 
