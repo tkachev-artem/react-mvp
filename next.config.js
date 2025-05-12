9f0f908913bf577d8134271bd96d7417eeb93560
artemtkachev <tkachev.tech@yandex.ru>
2025-05-13 00:56:24 +0300

    go

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    appDir: true,
+    // Добавьте здесь нужные экспериментальные функции
   },
   async redirects() {
     return [
